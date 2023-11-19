656ff0796be60bd80e47ecdab7b5390a4192e882
Mohammad Zaid <zaidcoder@gmail.com>
2023-11-19 16:44:16 +0530

    q1-a.cpp added

---
+++ q1/q1-a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int16_t dotProduct(const vector<int>& vector1, const vector<int>& vector2) 
+{	
+    int dot = 0;
+    for (size_t i = 0; i < vector1.size(); i++) 
+	{
+        dot += vector1[i] * vector2[i];
+    }
+    return dot;
+}
+
+int main() 
+{
+    vector<int> vector_a = {1, 2, 3};
+    vector<int> vector_b = {4, 5, 6};
+ 
+    int result = dotProduct(vector_a, vector_b);
+    cout << " dot product : " << result << endl;
+
+    return 0;
+}
